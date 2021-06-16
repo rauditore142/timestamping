0e25728a2574d1f61c22dcb0fadae1e86e25fe00
rauditore142 <clarkR10@cardiff.ac.uk>
2021-06-16 14:18:38 +0100

    Updated dt123 counter

--- g3_timestamp.m
+++ g3_timestamp.m
@@ -7,29 +7,30 @@ tic
 
 %hist settings
 %change these as required- bin size, offset, searching time
-t_max=1e6; %time in ps=1ms for searching function
-bin_size=5000; %in ps
+t_max=1e8; %time in ps=1ms for searching function
+d_bin=100;
+bin_size=50*d_bin; %in ps
 %t_offset=2.5e6 - bin_size; bunched
-t_offset=1.5e6 - bin_size; %antibunched
+t_offset=0;%1.5e6 - bin_size; %antibunched
 bins=round(t_max/bin_size);
 %full range
 histrange=linspace(-t_max,t_max,bins);
 N1=zeros(1,length(histrange)-1);
 N2=zeros(1,length(histrange)-1);
-N3=zeros(length(histrange),length(histrange));
+N3=zeros(length(histrange)-1,length(histrange)-1);
 %set up plots
 %full range plot
 % will have to use surf or contour?
 %pick a reference channel - eg C1 - and plot dx=dt(C1-C2), dy=dt(C1-C3)
-h=surf(histrange,histrange,N3);
+h=surf(histrange(1:length(N3)),histrange(1:length(N3)),N3);
 
 
 %% read in files
 %get file parameters such as count rate, number of events
 %enter file name here
-fileName1='bunched_test_g4_0.7V_10min_C1.bin';
-fileName2='bunched_test_g4_0.7V_10min_C2.bin';
-fileName3='bunched_test_g4_0.7V_10min_C3.bin';
+fileName1='0518_0.9V_100k_10min_C1.bin';
+fileName2='0518_0.9V_100k_10min_C2.bin';
+fileName3='0518_0.9V_100k_10min_C3.bin';
 %channel 1
 fid1 = fopen(fileName1,'r+');
 fseek(fid1,-8,'eof');%go to end of file
@@ -64,10 +65,11 @@ chunk1=(events1/chunk_factor);
 chunk2=(events2/chunk_factor);
 chunk3=(events3/chunk_factor);
 precision = 'uint64';
-chunk_plot=chunk_factor*1e2;
+chunk_plot=chunk_factor*1e3;
 N_exp=zeros(chunk_factor,1);
 co_exp=zeros(chunk_factor,1);
 t_int=zeros(chunk_factor,1);
+
 
 %return to beginning of file for reading chunks
 fseek(fid1,0,'bof');
@@ -86,15 +88,9 @@ for i1=1:chunk_factor
     indices=find(d_array1>st & d_array1<fin);
     ind=length(indices);
     %initialise arrays
-    dt12_array=zeros(ind,50); %preallocate dt for speed
-    dt13_array=zeros(ind,50);
-    %WHY DOESNT NANS WORK?
-    %dtsize=zeros(ind,1); %this will record length of dt i.e number of correlations
-    %mini=zeros(ind,1); %this will store the min dt in every loop
-%     t_int(i1)=(d_array1(end)+d_array2(end))/2;
-%     N_exp(i1)=rate1*rate2*binedge*t_int(i1);
-  
-    
+    dt12_array=NaN(ind,2000); %preallocate dt for speed
+    dt13_array=NaN(ind,2000);
+    dt123_array=NaN(ind,2000);
     for i2=1:ind
         here=indices(i2); %find index value of this iteration
         now=d_array1(here); %find time value of this index
@@ -103,68 +99,100 @@ for i1=1:chunk_factor
         %search in next channel using find
         match2 = find(d_array2<hi & d_array2>lo);
         if isempty(match2) == false
-            match3 = find(d_array3<hi & d_array3>lo);
-            if isempty(match3)==false
-                dt12=d_array2(match2) - now;
-                dt13=d_array3(match3) - now;
-                dt12_array(i2,1:length(dt12))=dt12;
-                dt13_array(i2,1:length(dt13))=dt13;
-                %dtsize(i2)=length(dt1); %size of dt array
-                %mini(i2)=min(abs(dt1)); %minimum dt
-            end 
+            dt12=d_array2(match2) - now;
+            dt12_array(i2,1:length(dt12))=dt12;
         end
-    end
-    
-    %remove nans
-    dt_plt12=dt12_array(dt12_array~=0);
-    dt_plt13=dt13_array(dt13_array~=0);
-    %dt_plt = dt(~isnan(dt));
-    
-    %count data
-    [n1, histrange]= histcounts(dt_plt12,histrange);%create histogram of pairs file- this command is a bottleneck on program speed, non linear with 'edges' size
-    N1= N1+n1;
-    [n2,histrange]=histcounts(dt_plt13,histrange);
-    N2=N2+n2;
-    
-   
-    for i3=1:length(histrange)-1 %column iterator
-        for i4=1:length(histrange)-1 %row iterator
-            pos1=N1(i3);
-            pos2=N2(i3);
-            if pos1~=0
-                if pos2~=0
-                    N3(i4,i3)=N1(i3)+N2(i3);
-                end
+        match3 = find(d_array3<hi & d_array3>lo);
+        if isempty(match3) == false
+            dt13=d_array3(match3) - now;
+            dt13_array(i2,1:length(dt13))=dt13;
+        end
+        row12=dt12_array(i2,:);
+        i3=length(row12);
+        row13=dt13_array(i2,:);
+        i4=length(row13);
+        row123=i3*i4;
+        for i12=1:i3
+            for i13=1:i4
+            dt123_array(i2,i12*i13)=dt12_array(i2,i12)-dt13_array(i2,i13);
             end
         end
     end
     
-    %write data to files
-    %     fileDir= 'C:\Users\rache\Documents\Uni\MATLAB\files';
-    %     fullfileName=fullfile(fileDir,'dt.dat');
-    %     dlmwrite(fullfileName,dt,'-append');
-    %     fullfileName=fullfile(fileDir,'dt_size.dat');
-    %     dlmwrite(fullfileName,dtsize,'-append');
-    %     fullfileName=fullfile(fileDir,'dt_min.dat');
-    %     dlmwrite(fullfileName,mini,'-append');
+    %count data
+    [n1, histrange]= histcounts(dt12_array,histrange);%create histogram of pairs file- this command is a bottleneck on program speed, non linear with 'edges' size
+    N1= N1+n1;
+    [n2,histrange]=histcounts(dt13_array,histrange);
+    N2=N2+n2;
     
-    
+    [n3,histrange,histrange]=histcounts2(dt123_array(:,)dt12_array,dt13_array);
+    for i3=1:length(histrange)-1 %column iterator
+        for i4=1:length(histrange)-1 %row iterator
+            N3(i4,i3)=N3(i4,i3)+n3(i4,i3);
+        end
+    end
+   
     %% update plot
     i3=mod(i1,round(chunk_plot/chunk_factor));
     if i3==0%only update plot every time chunk_plot_size has been processed- faster
-        h.ZDataSource = 'N3';
+        h.ZDataSource = 'n3';
         refreshdata;
         shg;
     end
     
-    grad2 = ftell(fid2)/max(d_array2);%gradient of fid2_file_pos at end of this chunk/channel2(time)
-    new_posn_2 = 8*round(max(d_array1)*grad2/8);%each timestamp has 8 bytes so find time to nearest 8 units
-    fseek(fid2,new_posn_2,'bof');%move file current position to new_posn_B
-    grad3=ftell(fid3)/max(d_array3);
-    new_posn_3=8*round(max(d_array2)*grad2/8);
-    fseek(fid3,new_posn_3,'bof');
+    T2=isempty(d_array2);
+    T3=isempty(d_array3);
+    
+    if T2 == 1 | T3 == 1
+        break
+    
+    elseif T2 == 0 || T3 == 0
+        grad2 = ftell(fid2)/max(d_array2);%gradient of fid2_file_pos at end of this chunk/channel2(time)
+        new_posn_2 = 8*round(max(d_array1)*grad2/8);%each timestamp has 8 bytes so find time to nearest 8 units
+        fseek(fid2,new_posn_2,'bof');%move file current position to new_posn_B
+        grad3=ftell(fid3)/max(d_array3);
+        new_posn_3=8*round(max(d_array2)*grad2/8);
+        fseek(fid3,new_posn_3,'bof');
+    end
+    
+   
 end
 
-
+% fullfileName=fullfile(fileDir,'N12.dat');
+% dlmwrite(fullfileName,N1,'-append');
+% fullfileName=fullfile(fileDir,'N13.dat');
+% dlmwrite(fullfileName,N2,'-append');
+    
 fclose('all');
 toc
+
+
+%dtsize(i2)=length(dt1); %size of dt array
+    %mini(i2)=min(abs(dt1)); %minimum dt
+    %this needs to work such that t12 t13 are same size
+    %rather than reinvent hist2, reinvent this calculation step
+    
+    %remove nans
+    %dt_plt12=dt12_array(dt12_array~=0);
+    %dt_plt13=dt13_array(dt13_array~=0);
+    
+    
+
+
+    %write data to files
+%     fileDir= 'C:\Users\rache\Documents\Uni\postgrad\MATLAB';
+%     fullfileName=fullfile(fileDir,'dt12.dat');
+%     dlmwrite(fullfileName,dt_plt12,'-append');
+%     fullfileName=fullfile(fileDir,'dt13.dat');
+%     dlmwrite(fullfileName,dt_plt13,'-append');
+
+
+%     for i3=1:length(histrange)-1 %column iterator
+%         for i4=1:length(histrange)-1 %row iterator
+%             pos1=N1(i4); %N1 = C1-C2, N2 = C1-C3
+%             pos2=N2(i3); %row needs to be C1-C2, column C1-C3
+%             if pos1~=0 && pos2~=0
+%                 N3(i4,i3)=N1(i4)+N2(i3);
+%             end
+%         end
+%     end
